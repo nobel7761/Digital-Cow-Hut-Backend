81038eb2d62b61683cf2c413fb1e05383c5c1368
nobel7761 <habiburnobel@gmail.com>
2023-07-03 01:28:55 +0600

    all types of error handling is completed

---
+++ src/errors/handleZodError.ts
@@ -0,0 +1,23 @@
+import httpStatus from 'http-status';
+import { ZodError, ZodIssue } from 'zod';
+import { IGenericErrorResponse } from '../interfaces/common';
+import { IGenericErrorMessage } from '../interfaces/error';
+
+const handleZodError = (error: ZodError): IGenericErrorResponse => {
+  const errors: IGenericErrorMessage[] = error.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue?.path[issue.path.length - 1],
+      message: issue?.message,
+    };
+  });
+
+  const statusCode = httpStatus.BAD_REQUEST; //400
+
+  return {
+    statusCode,
+    message: 'Zod Error',
+    errorMessages: errors,
+  };
+};
+
+export default handleZodError;
